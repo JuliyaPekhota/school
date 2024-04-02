ab0ce3d517dbd91cb00a2012bd383782ef73c6ee
Juliya Pekhota <konyliyaevg@gmail.com>
2024-04-02 13:21:53 +0300

    website school

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+const imgs = document.querySelectorAll(".carousel__image");
+
+document.addEventListener('DOMContentLoaded', function() {
+    let splide = new Splide('.splide');
+
+    splide.mount();
+});
